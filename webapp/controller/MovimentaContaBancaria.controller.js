5c4795aa57360ac61f5d2c03aa56ebfb2dc4c5ec
matusca96 <gomesloud@gmail.com>
2019-05-03 10:52:38 -0300

    Atualização.

--- webapp/controller/MovimentaContaBancaria.controller.js
+++ webapp/controller/MovimentaContaBancaria.controller.js
@@ -15,14 +15,19 @@ sap.ui.define([
 			this.getOwnerComponent().setModel(oParamModel, "parametros");
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 			
-			this.getModel().attachMetadataLoaded(function(){
-				var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
-				var oView = this.getView();
-				var oTable = oView.byId("tableMovimento");
-				var oColumn = oView.byId("columnConta");
-				
-				oTable.sort(oColumn);
-				oView.byId("tableMovimento").getBinding("rows").filter(oFilter, "Application");
+			var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+			var oView = this.getView();
+			var oTable = oView.byId("tableMovimento");
+			
+			oTable.bindRows({
+				path: '/MovimentoContaBancarias',
+				sorter: {
+					path: 'ContaBancariaDetails/Descricao'
+				},
+				parameters: {
+					expand: 'ContaBancariaDetails'
+				},
+				filters: oFilter 
 			});
 		},
 		
